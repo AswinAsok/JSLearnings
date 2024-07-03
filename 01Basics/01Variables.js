1e4aaed89842bb80288a3c916a6275fcdc621f89
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-03 10:17:55 +0530

    conversion-of-types

---
+++ 01Basics/01Variables.js
@@ -0,0 +1,13 @@
+const accountId = 559659;
+let accountEmail = "heynahi@gmail.com";
+var accountPassword = "";
+
+accountCity = "India";
+
+// accountId = 2; /are you mad? you can't change the value of a constant variable
+
+accountEmail = "ac@ac.com";
+accountPassword = 233233;
+accountCity = "USA";
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
---
+++ 01Basics/02DataType.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version of JS
+
+// alert("Hello World");
+
+console.log(3 + 3);
+console.log("Unnikuttan");
+
+let name = "hitesh"; //ho gaya string
+let age = 22; // ho gaya number
+let isApproved = true; // ho gaya boolean
+
+//number => range bhi hai => 2^53 - 1
+//bigint => range bhi hai => 2^53 - 1 se zyada
+//string => " " ya ' ' mei
+//boolean => true or false
+//null standalone value hei
+//undefined standalone value hei
+//symbol unique value hei
+
+
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof 22);
+console.log(typeof null);
+console.log(typeof undefined);
+
