98ebc9e09729a49d60f0000eed2fb53d7141e4a0
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-05 16:40:34 +0530

    math-numbers

---
+++ 01Basics/06NumsAndMath.js
@@ -0,0 +1,23 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.89666;
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+/// ++++++++++++++++++++ Maths +++++++++++++++++
+
+console.log(Math);
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.3));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
