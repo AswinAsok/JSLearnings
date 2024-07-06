e80ec162d362f8e253b7e7c0b7c5567c002a2c37
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-06 10:27:38 +0530

    feat(date)

---
+++ 01Basics/07datesInJs.js
@@ -0,0 +1,23 @@
+//Dates
+
+let myDate = new Date();
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date(2023, 11, 23, 5, 3);
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(Math.floor(Date.now() / 1000));
+
+let newDate = new Date();
+console.log(newDate.getMonth());            
+
+
+
