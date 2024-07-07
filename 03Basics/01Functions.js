902b536c0f20a10a38e4fbd6252112962b611adc
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-07 10:39:36 +0530

    feat(functions)

---
+++ 03Basics/01Functions.js
@@ -0,0 +1,45 @@
+function sayMyName() {
+    console.log("H");
+    console.log("E");
+    console.log("L");
+    console.log("L");
+    console.log("O");
+}
+
+// sayMyName();
+
+// function addTwoNumbers(a, b) {
+//     console.log(a + b);
+// }
+
+function addTwoNumbers(a, b) {
+    let result = a + b;
+    return result;
+}
+
+const result = addTwoNumbers(10, 5);
+// console.log("Result ki jo value hei: ", result);
+
+// function loginUserMessage(username) {
+//     if (username === undefined) {
+//         return "Please provide a username";
+//     }
+//     return `${username} just logged in`;
+// }
+
+// console.log(loginUserMessage());
+
+// function calculateCartPrice(va1, val2, ...num1) {
+//     return num1;
+// }
+
+// console.log(calculateCartPrice(10, 100, 1000, 10000));
+
+// const user = {
+//     username: "Hitesh",
+//     price: "1100",
+// };
+
+// function handleObject(anyObject) {
+//     console.log(`Username: ${anyObject.username}`);
+// }
