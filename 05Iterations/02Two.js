3f873e90cf13a79465433174d41ca8a0915d8ab6
Aswin Asok <aswinasokofficial@gmail.com>
2024-07-09 17:52:55 +0530

    learn(iterators):for-while-do

---
+++ 05Iterations/02Two.js
@@ -0,0 +1,21 @@
+let index = 0;
+// while (index <= 10) {
+//     console.log(`Value of Index : ${index}`);
+
+//     index += 2;
+// }
+
+// let myArray = ["Flassh", "Batman", "Superman"];
+
+// let arrIndex = 0;
+// while (arrIndex < myArray.length) {
+//     console.log(`Value is ${myArray[arrIndex]}`);
+//     arrIndex++;
+// }
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
