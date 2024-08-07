656ee4fa574ab93208fe0345bf14d3a862157bff
Aswin Asok <aswinasokofficial@gmail.com>
2024-08-07 20:37:46 +0530

    feat(linkedList):insertion-start-end

--- DSAJS/LinkedList/LinkedList.js
+++ DSAJS/LinkedList/LinkedList.js
@@ -10,3 +10,25 @@ class LinkedList {
         this.head = null;
     }
 }
+
+//Insert at the start
+LinkedList.prototype.insertAtBeginning = function (data) {
+    const newNode = new Node(data);
+    this.head = newNode;
+};
+
+//Insert at the end
+LinkedList.prototype.insertAtEnd = function (data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+        this.head = newNode;
+        return;
+    } else {
+        let last = this.head;
+        while (last.next) {
+            last = last.next;
+        }
+
+        last.next = newNode;
+    }
+};
