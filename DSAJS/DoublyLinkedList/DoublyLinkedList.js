efebecfe546c0f5282d24c697ef7ccc41a82d505
Aswin Asok <aswinasokofficial@gmail.com>
2024-08-09 21:08:05 +0530

    feat(DlinkedList):insertAtBeginning

---
+++ DSAJS/DoublyLinkedList/DoublyLinkedList.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(data, next = null, prev = null) {
+        this.data = data;
+        this.next = next;
+        this.prev = prev;
+    }
+}
+
+class DoublyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+}
+
+DoublyLinkedList.prototype.insertAtBeginning = function (data) {
+    const newNode = new Node(data, this.head, null);
+
+    if (!this.head) {
+        this.head.prev = newNode;
+    }
+    this.head = newNode;
+
+    if (this.tail === null) {
+        this.tail = newNode;
+    }
+};
