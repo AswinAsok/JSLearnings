ea039b57f6d46a764fd6f830be03f6c0b17023ab
Aswin Asok <aswinasokofficial@gmail.com>
2025-06-24 07:57:22 +0530

    loops-challenges

--- UdemyJS/part4/loopChallenges.js
+++ UdemyJS/part4/loopChallenges.js
@@ -15,13 +15,28 @@
 
 // console.log(countdown);
 
-let teaCollection = [];
-let tea;
+// let teaCollection = [];
+// let tea;
 
-do {
-    tea = prompt(`Enter your favorite tea(type "stop" to finish)`);
+// do {
+//     tea = prompt(`Enter your favorite tea(type "stop" to finish)`);
 
-    if (tea !== "stop") {
-        teaCollection.push(tea);
-    }
-} while (tea !== "stop");
+//     if (tea !== "stop") {
+//         teaCollection.push(tea);
+//     }
+// } while (tea !== "stop");
+
+// let total = 0;
+// let k = 1;
+
+// do {
+//     total += k;
+// } while (k <= 3);
+
+let multipliedNumber = [];
+let numbers = [2, 4, 6];
+for (let i = 0; i < numbers.length; i++) {
+    multipliedNumber.push(numbers[i] * 2);
+}
+
+console.log(multipliedNumber);
